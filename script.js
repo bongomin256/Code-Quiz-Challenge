745efadcc053d9aad16558ddefae8e5ed41b8e40
Godfrey Bongomin <bongomin@uw.edu>
2022-03-19 20:26:59 -0700

    upadting the script with displayqiz, clickedAnswer and displayHighscore functions

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function startGame(){
     welcomeSection.setAttribute("style", "display: none" )
     questionSection.style.display = "block"
 
+    //calling the startTimer and displayQuiz function
     startTimer();
     displayQuiz();
 };
@@ -49,6 +50,77 @@ function startTimer(){
 
     }, 1000)
 };
+
+//creating the function to update the question section with the questions.
+var currentQuiz = 0;
+var lastQuiz = false;
+
+function displayQuiz() {
+
+    if(currentQuiz == questions.length -1) {
+        lastQuiz = true;
+    }
+    
+    // updating the question section with the current question
+    quiz.textContent = questions[currentQuiz].Question;
+    option1.textContent =questions[currentQuiz].Options[0];
+    option2.textContent =questions[currentQuiz].Options[1];
+    option3.textContent =questions[currentQuiz].Options[2];
+    option4.textContent =questions[currentQuiz].Options[3];
+}
+
+function clickedAnswer(event) {
+  
+    //checking if the right answer.
+    if(event.target.textContent === questions[currentQuiz].Answer){
+        timerEL.textContent = timerCount;
+    } else {
+        timerCount-=2;
+        timerEL.textContent = timerCount;
+    }
+    
+     //checking if it the last question
+    if(lastQuiz) {
+        //Setting the remaining time left to be the scores
+        scoreText.textContent = timerCount;
+
+        //making the score section to be display after and hiding question and highscores of the page
+        questionSection.style.display = "none"
+        scoreSection.style.display = "block"
+        highscoreEl.style.display = "none"
+        // stopping the timer if it is the last quiestion
+        timerCount = 0
+    } else {
+        // updating the curent question with the next question to display
+        currentQuiz = currentQuiz + 1;
+        displayQuiz();
+    }  
+};
+
+//setting up a displayHighscores function
+function displayHighscores(event) {
+
+    var userInitial = document.querySelector('#tex-initial').value
+    var userScores = timerCount.value
+
+    // storing the inital and the score in the local storage
+    localStorage.setItem("userInitial", userInitial);
+    localStorage.setItem("userScores", userScores);
+
+    event.preventDefault();
+    event.stopPropagation();
+    
+    // setting the display of question and score section to none and displaying highscore page
+    questionSection.style.display = "none"
+    scoreSection.style.display = "none"
+    highscoreEl.style.display = "block"
+
+    //get the store data from the local storage
+    document.querySelector('.initial').textContent =localStorage.getItem("userInitial");
+    document.getElementById('finalscore').textContent = localStorage.getItem("userScores");
+
+};
+
 
 
 
