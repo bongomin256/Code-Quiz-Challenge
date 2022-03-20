63049745db340d998befaeb5fbe67ef812d8722d
Godfrey Bongomin <bongomin@uw.edu>
2022-03-19 20:04:33 -0700

    Updating the script file with init, startgame and startTimer function

--- script.js
+++ script.js
@@ -11,8 +11,45 @@ var welcomeSection = document.querySelector('.welcome')
 var questionSection = document.querySelector('.question-Container')
 var scoreSection = document.querySelector('.done')
 
+var highscoreEl = document.querySelector('.highscores')
 var submitBtn = document.querySelector('.submit-btn')
 var scoreText = document.querySelector('#score-text')
 
 var timer;
 var timerCount;
+
+//creating the init function for managing page load
+function init() {
+    highscoreEl.style.display = "none";
+    questionSection.style.display = "none";
+    scoreSection.style.display = "none";
+};
+
+// creating startGame function for when the start button is clicked
+function startGame(){
+    timerCount = 30;
+    startButton.disabled = true;
+    welcomeSection.setAttribute("style", "display: none" )
+    questionSection.style.display = "block"
+
+    startTimer();
+    displayQuiz();
+};
+
+// creating startTimer for to start the timer
+function startTimer(){
+    timer = setInterval(function() {
+        timerCount--;
+    
+        if(timerCount <= 0){
+            clearInterval(timer)
+        } else{
+            timerEL.textContent = timerCount;
+        }
+
+    }, 1000)
+};
+
+
+
+init();
