5a5dabab6bb68629a849dbd9e9b2f450d6942943
Godfrey Bongomin <bongomin@uw.edu>
2022-03-19 19:22:12 -0700

    Updating the script file with the global variable created

---
+++ script.js
@@ -0,0 +1,18 @@
+// Creating the global variables
+var timerEL = document.querySelector('#timer')
+var startButton = document.querySelector('.start-btn')
+var quiz = document.querySelector('#questions')
+var option1 = document.querySelector('#option1')
+var option2 = document.querySelector('#option2')
+var option3 = document.querySelector('#option3')
+var option4 = document.querySelector('#option4')
+
+var welcomeSection = document.querySelector('.welcome')
+var questionSection = document.querySelector('.question-Container')
+var scoreSection = document.querySelector('.done')
+
+var submitBtn = document.querySelector('.submit-btn')
+var scoreText = document.querySelector('#score-text')
+
+var timer;
+var timerCount;
